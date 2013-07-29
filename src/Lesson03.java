f6978002e24183e6edb2864fc175521d599fc73c
Eugene Fidelin <eugene.fidelin@gmail.com>
2013-07-29 19:54:18 +0200

    format sourcecode

--- src/Lesson03.java
+++ src/Lesson03.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.security.*;
+
 import static java.lang.Math.ceil;
 
 /**
@@ -8,42 +9,42 @@ import static java.lang.Math.ceil;
  * contents of F using a collision resistant hash and then distribute the resulting short hash value h=H(F) to users via
  * some authenticated channel (later on we will use digital signatures for this). Browsers would download the entire
  * file F, check that H(F) is equal to the authentic hash value h and if so, display the video to the user.
- *
+ * <p/>
  * Unfortunately, this means that the video will only begin playing after the *entire* file F has been downloaded.
  * Our goal in this project is to build a file authentication system that lets browsers authenticate and play video
  * chunks as they are downloaded without having to wait for the entire file.
- *
+ * <p/>
  * Instead of computing a hash of the entire file, the web site breaks the file into 1KB blocks (1024 bytes).
  * It computes the hash of the last block and appends the value to the second to last block. It then computes the hash
  * of this augmented second to last block and appends the resulting hash to the third block from the end.
- *
+ * <p/>
  * The final hash value h0 – a hash of the first block with its appended hash – is distributed to users via the
  * authenticated channel as above.
- *
+ * <p/>
  * Now, a browser downloads the file F one block at a time, where each block includes the appended hash value from the
  * diagram above. When the first block (B0 ∥∥ h1) is received the browser checks that H(B0 ∥∥ h1) is equal to h0 and if
  * so it begins playing the first video block. When the second block (B1 ∥∥ h2) is received the browser checks that
  * H(B1 ∥ h2) is equal to h1 and if so it plays this second block. This process continues until the very last block.
  * This way each block is authenticated and played as it is received and there is no need to wait until the entire
  * file is downloaded.
- *
+ * <p/>
  * It is not difficult to argue that if the hash function H is collision resistant then an attacker cannot modify any
  * of the video blocks without being detected by the browser. Indeed, since h0=H(B0 ∥∥ h1) an attacker cannot find a
  * pair (B′0,h′1)≠(B0,h1) such that h0=H(B0 ∥∥ h1) since this would break collision resistance of H. Therefore after
  * the first hash check the browser is convinced that both B0 and h1 are authentic. Exactly the same argument proves
  * that after the second hash check the browser is convinced that both B1 and h2 are authentic, and so on for the
  * remaining blocks.
- *
+ * <p/>
  * In this project we will be using SHA256 as the hash function. When appending the hash value to each block, please
  * append it as binary data, that is, as 32 unencoded bytes (which is 256 bits). If the file size is not a multiple of
  * 1KB then the very last block will be shorter than 1KB, but all other blocks will be exactly 1KB.
  */
 public class Lesson03 {
-    final protected static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    final protected static char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     final protected static int buff = 1024;
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, IOException  {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         RandomAccessFile file = new RandomAccessFile("D:\\example.mp4", "r");
         MessageDigest hashSum = MessageDigest.getInstance("SHA-256");
 
@@ -52,7 +53,7 @@ public class Lesson03 {
 
         byte[] digest = null;
 
-        for (int i = blocksCount-1; i >= 0; i--) {
+        for (int i = blocksCount - 1; i >= 0; i--) {
             file.seek(i * buff);
             int blockSize = (i * buff + buff > offset) ? (int) (offset - i * buff) : buff;
             byte[] buffer = new byte[blockSize];
@@ -67,13 +68,13 @@ public class Lesson03 {
 
         file.close();
 
-        System.out.println(byteArrayToHexString(digest)) ;
+        System.out.println(byteArrayToHexString(digest));
     }
 
     public static String byteArrayToHexString(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         int v;
-        for ( int j = 0; j < bytes.length; j++ ) {
+        for (int j = 0; j < bytes.length; j++) {
             v = bytes[j] & 0xFF;
             hexChars[j * 2] = hexArray[v >>> 4];
             hexChars[j * 2 + 1] = hexArray[v & 0x0F];
@@ -84,7 +85,7 @@ public class Lesson03 {
     public static byte[] concat(byte[] a, byte[] b) {
         int aLen = a.length;
         int bLen = b.length;
-        byte[] c = new byte[aLen+bLen];
+        byte[] c = new byte[aLen + bLen];
         System.arraycopy(a, 0, c, 0, aLen);
         System.arraycopy(b, 0, c, aLen, bLen);
         return c;
--- src/Lesson04.java
+++ src/Lesson04.java
@@ -7,69 +7,68 @@ import java.util.Arrays;
  * crypto-class.appspot.com. Padding oracle vulnerabilities affect a wide variety of products, including secure tokens.
  * This project will show how they can be exploited. We discussed CBC padding oracle attacks in Lecture 7.6, but if you
  * want to read more about them, please see Vaudenay's paper.
- *
+ * <p/>
  * Now to business. Suppose an attacker wishes to steal secret information from our target web site
  * crypto-class.appspot.com. The attacker suspects that the web site embeds encrypted customer data in URL parameters
  * such as this:
- *
+ * <p/>
  * http://crypto-class.appspot.com/po?er=f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4
- *
+ * <p/>
  * That is, when customer Alice interacts with the site, the site embeds a URL like this in web pages it sends to Alice.
  * The attacker intercepts the URL listed above and guesses that the ciphertext following the "po?er=" is a hex encoded
  * AES CBC encryption with a random IV of some secret data about Alice's session.
- *
+ * <p/>
  * After some experimentation the attacker discovers that the web site is vulnerable to a CBC padding oracle attack. In
  * particular, when a decrypted CBC ciphertext ends in an invalid pad the web server returns a 403 error code (forbidden
  * request). When the CBC padding is valid, but the message is malformed, the web server returns a 404 error code (URL not found).
- *
+ * <p/>
  * Armed with this information your goal is to decrypt the ciphertext listed above. To do so you can send arbitrary HTTP
  * requests to the web site of the form http://crypto-class.appspot.com/po?er="your ciphertext here" and observe the
  * resulting error code. The padding oracle will let you decrypt the given ciphertext one byte at a time. To decrypt a
  * single byte you will need to send up to 256 HTTP requests to the site. Keep in mind that the first ciphertext block
  * is the random IV. The decrypted message is ASCII encoded.
- *
+ * <p/>
  * To get you started here is a short Python script that sends a ciphertext supplied on the command line to the site and
  * prints the resulting error code. You can extend this script (or write one from scratch) to implement the padding
  * oracle attack. Once you decrypt the given ciphertext, please enter the decrypted message in the box below.
- *
+ * <p/>
  * This project shows that when using encryption you must prevent padding oracle attacks by either using encrypt-then-MAC
  * as in EAX or GCM, or if you must use MAC-then-encrypt then ensure that the site treats padding errors the same way it
  * treats MAC errors.
  */
 public class Lesson04 {
-    final protected static char[] HEX_ARRAY = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    final protected static char[] HEX_ARRAY = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     public static final int BLOCK_SIZE = 16;
     public static final String CIPHER_TEXT = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4";
     public static final String ATTACK_URL = "http://crypto-class.appspot.com/po?er=";
 
-    public static void main(String[] args) throws IOException  {
-
+    public static void main(String[] args) throws IOException {
         // cipher text
         byte[] c = hexStringToByteArray(CIPHER_TEXT);
         // decrypted text
         String d = "";
 
         for (int block = 0; block < (c.length - BLOCK_SIZE) / BLOCK_SIZE; block++) {
-            byte[] cb =  Arrays.copyOfRange(c, 0, c.length-block* BLOCK_SIZE);
+            byte[] cb = Arrays.copyOfRange(c, 0, c.length - block * BLOCK_SIZE);
 
-            for(int i = 0; i < BLOCK_SIZE; i++) {
+            for (int i = 0; i < BLOCK_SIZE; i++) {
 
                 byte[] cm = cb.clone();
                 byte found = 0;
 
                 // position of byte that will be guessed
-                int pos = cb.length-1 - BLOCK_SIZE - i;
+                int pos = cb.length - 1 - BLOCK_SIZE - i;
 
                 // try to guess value of this byte
-                for(int b = 0; b < 256; b++) {
+                for (int b = 0; b < 256; b++) {
 
                     // pad all bytes from current to the end of the block
-                    for(int k = 0; k < i+1; k++){
-                        cm[pos + k] = (byte)(cb[pos + k] ^ (i+1));
+                    for (int k = 0; k < i + 1; k++) {
+                        cm[pos + k] = (byte) (cb[pos + k] ^ (i + 1));
                     }
 
                     // xor byte with guessed value
-                    cm[pos] = (byte)(cm[pos] ^ b);
+                    cm[pos] = (byte) (cm[pos] ^ b);
 
                     int status = getUrlStatus(ATTACK_URL + byteArrayToHexString(cm));
 
@@ -86,15 +85,15 @@ public class Lesson04 {
                         // pad ^ guessed = 0
                         // save this value as possible result
                         // but wait if 404 is returned
-                        found = (byte)b;
+                        found = (byte) b;
                     }
                 }
 
                 // save found value for next round
-                cb[pos] = (byte)(cb[pos] ^ found);
+                cb[pos] = (byte) (cb[pos] ^ found);
 
                 // save to decrypted value
-                d = (char)found + d;
+                d = (char) found + d;
 
                 // Log decrypted value
                 System.out.println("found >> " + found + " = " + d);
@@ -104,12 +103,12 @@ public class Lesson04 {
     }
 
     public static int getUrlStatus(String urlPath) throws IOException {
-        URL url = new URL (urlPath);
+        URL url = new URL(urlPath);
         URLConnection connection = url.openConnection();
 
         connection.connect();
 
-        if ( connection instanceof HttpURLConnection) {
+        if (connection instanceof HttpURLConnection) {
             HttpURLConnection httpConnection = (HttpURLConnection) connection;
 
             return httpConnection.getResponseCode();
@@ -123,7 +122,7 @@ public class Lesson04 {
         byte[] data = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
             data[i / 2] = (byte) ((Character.digit(s.charAt(i), BLOCK_SIZE) << 4)
-                    + Character.digit(s.charAt(i+1), BLOCK_SIZE));
+                    + Character.digit(s.charAt(i + 1), BLOCK_SIZE));
         }
         return data;
     }
@@ -131,7 +130,7 @@ public class Lesson04 {
     public static String byteArrayToHexString(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         int v;
-        for ( int j = 0; j < bytes.length; j++ ) {
+        for (int j = 0; j < bytes.length; j++) {
             v = bytes[j] & 0xFF;
             hexChars[j * 2] = HEX_ARRAY[v >>> 4];
             hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
